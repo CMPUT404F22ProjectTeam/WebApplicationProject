ec96f7d9ada8dfaa0eb7a707cc733bf2c41138d6
Damian Li <77185597+kkli08@users.noreply.github.com>
2022-10-20 16:11:23 -0600

    backend

---
+++ backend/socialdistribution/models.py
@@ -0,0 +1,99 @@
+from email.policy import default
+from operator import mod
+from tabnanny import check
+from turtle import title
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import PermissionsMixin
+
+
+# Create your models here.
+class Author(AbstractUser):
+
+    type = "author"
+
+    id = models.URLField(primary_key=True, max_length=255)
+    host = models.URLField(max_length=255)
+    displayName = models.CharField(max_length=255, default = '')
+    url = models.URLField(max_length=255)
+    github = models.URLField(required=False, blank=True)
+    profileImage = models.ImageField(required=False, blank=True)
+
+
+class FollowRequest(models.Model):
+
+    type = "Follow"
+    summary = models.CharField(max_length=255)
+    # actor sent request to object's inbox
+    actor = models.CharField(max_length=255)
+    object = models.CharField(max_length=255)
+
+
+class Friend(models.Model):
+
+    type = "Friend"
+
+    actor = models.CharField(max_length=255)
+    # -----
+    object = models.JSONField(default=[])
+
+
+class Liked(models.Model):
+
+    type = "liked"
+
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    items = models.JSONField(default=[])
+
+
+class Likes(models.Model):
+
+    type = "Like"
+
+    context = models.CharField(max_length=255,  default="")
+    summary = models.CharField(max_length=255, default="")
+    author = models.URLField(max_length=255, default="")
+    object = models.URLField(max_length=255, default="")
+    
+
+class Inbox(models.Model):
+    
+    type = "Inbox"
+    author = models.CharField(max_length=256, default="")
+    message = models.JSONField(null=True)
+
+
+
+class Comment(models.Model):
+
+    type = "comment"
+    
+    author = models.CharField(max_length=60)
+    comment = models.TextField()
+    contentType = models.CharField(max_length=60)
+    published = models.DateTimeField(default=timezone.now)
+    id = models.URLField(primary_key=True, max_length=255)
+
+
+class Post(models.Model):
+
+    type = "post"
+
+    title = models.CharField(max_length=255, default = '')
+    id = models.URLField(primary_key=True, max_length=255)
+    source = models.URLField(max_length=255)
+    origin = models.URLField(max_length=255)
+    description = models.TextField(max_length=255, default = '')
+    contentType = models.CharField(max_length=60)
+    content = models.TextField(blank=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    #categories
+    published = models.DateTimeField(default=timezone.now)
+    count = models.ImageField(default=0, blank=True)
+    visibility = models.CharField(default="PUBLIC")
+    unlisted = models.BooleanField(default="False")
+
+
+
+
