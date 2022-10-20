d346c7be6c866a6a72d99bf5ccae4c73e997d703
kkli08 <ke_2021@126.com>
2022-10-20 16:12:43 -0600

    sd deleted

--- socialdistribution/authors/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def index(request):    # request
-    return HttpResponse("authors works :)")     # response
--- socialdistribution/posts/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):    # request
-    return HttpResponse("views works :)")     # response
