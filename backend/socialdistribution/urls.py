961c89b901f756c08e5299895a09f2dd0b5288a6
kkli08 <ke_2021@126.com>
2022-10-24 15:03:16 -0600

    update post Oct 24 15:00

--- backend/socialdistribution/urls.py
+++ backend/socialdistribution/urls.py
@@ -8,7 +8,6 @@ from .viewsets.comment_views import CommentViewSet
 
 urlpatterns = [
     path('authors/', AuthorViewSet.as_view({'put': 'sign_up'})),
-    path('posts/', PostViewSet.as_view),
-    path('comments/', CommentViewSet.as_view),
-    
+    path('author/<str:author_id>/posts/', PostViewSet.as_view({'get': 'getlist', 'post': 'create'})),
+    path('author/<str:author_id>/posts/<str:post_id>/', PostViewSet.as_view({'get': 'get', 'put': 'put', 'post': 'update', 'delete': 'delete'})),
 ]
