0a4f7240f089b0502d014a0a3040a20068777846
Yanzhi Huang <yanzhihuang129@gmail.com>
2022-10-24 17:12:52 -0600

    Update admin.py

--- backend/socialdistribution/admin.py
+++ backend/socialdistribution/admin.py
@@ -2,11 +2,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-class AuthorAdminSite(admin.ModelAdmin):
-    model = Author
-    feilds = ['admin_permission']
-
-admin.site.register(Author, AuthorAdminSite)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Author)
