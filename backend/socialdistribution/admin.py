3daead2c75359f3c86f6bb5a996b60d74b3034cf
kkli08 <ke_2021@126.com>
2022-10-24 16:40:22 -0600

    Damian commit! update admin.py

--- backend/socialdistribution/admin.py
+++ backend/socialdistribution/admin.py
@@ -5,4 +5,10 @@ from .models import *
 
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Author)
+admin.site.register(FollowRequest)
+admin.site.register(Friend)
+admin.site.register(Liked)
+admin.site.register(Likes)
+admin.site.register(Inbox)
 
