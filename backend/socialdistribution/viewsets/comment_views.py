22fcecf588eb5a35265e1353ba2aa87dcd5a0b2f
Yanzhi Huang <yanzhihuang129@gmail.com>
2022-10-25 23:49:57 -0600

    finish create a comment

--- backend/socialdistribution/viewsets/comment_views.py
+++ backend/socialdistribution/viewsets/comment_views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
 from django.http import JsonResponse
+from socialdistribution.serializers import AuthorSerializer
 from socialdistribution.models import *
 import uuid
 import datetime
@@ -15,32 +16,31 @@ GET [local, remote] get the list of comments of the post whose id is POST_ID (pa
 POST [local] if you post an object of “type”:”comment”, it will add your comment to the post whose id is POST_ID
 '''
 
-HOST = 'http://localhost:8000'
+HOST = 'http://127.0.0.1:8000'
 
 def real_post_id(request):
     url = request.build_absolute_uri()[:-1]
-    post_id = url.split('/')[4]
+    post_id = url.split('/')[6]
     return post_id
 
 def current_id(request):
     url = request.build_absolute_uri()[:-1]
-    author_id = url.split('/')[2]
-    current_author_id = f'HOST/authors/{author_id}'
+    author_id = url.split('/')[4]
+    current_author_id = HOST + f'/authors/{author_id}'
     return current_author_id
 
 
-# URL: ://service/authors/{AUTHOR_ID}/posts/{POST_ID}/comments POST Method
+
 class CommentViewSet(viewsets.ModelViewSet):
     
+    # URL: ://service/authors/{AUTHOR_ID}/posts/{POST_ID}/comments POST Method
+    def create_comment(self, request, author_id, post_id):
+        # print(author_id) #404hhh
+        # print(post_id) #edb04567-c77f-4886-b87d-797bc5ce3ad1
 
-    def create_comment(self, request, author_id):
-        
-        # get post and author id
-        current_author_id = current_id(request)
-        post_id = real_post_id(request)
+        # get data from request
+        current_author_id = current_id(request) #http://localhost:8000/authors/404hhh
         comment_content = request.data.get('content')
-        # print(current_author_id)
-        # print(post_id)
 
         # create the data for comment
         publish_time = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
@@ -48,10 +48,32 @@ class CommentViewSet(viewsets.ModelViewSet):
         comment_type = "text/markdown"
 
         #get post origin author
-        post = Post.objects.get(id=post_id)
-        
+        post = Post.objects.get(uuid=post_id)
+        comment_id = post.id + comment_uuid #http://127.0.0.1:8000/authors/test000/posts/edb04567-c77f-4886-b87d-797bc5ce3ad1
+        current_author = Author.objects.get(id = current_author_id)
+        author_info = AuthorSerializer(current_author)
+        author_json = author_info.data
 
-        return response()
+        #save in database and response message
+        Comment.objects.create(id=comment_id, author=current_author_id, comment=comment_content,
+        contentType=comment_type, published=publish_time)
+        response_msg = {"type":"comment",
+        "author": author_json,
+        "comment": comment_content,
+        "contentType": comment_type,
+        "published": publish_time,
+        "id": comment_id}
+
+        return JsonResponse(response_msg)
+
+    # URL://service/authors/{AUTHOR_ID}/posts/{POST_ID}/comments 
+    # GET Method
+    def all_post_comments(self, request, author_id, post_id):
+        pass
+
+
+
+        
 
 
     
