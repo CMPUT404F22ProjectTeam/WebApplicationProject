e362a1b32c0e1fb02d452744e472d3fffc310310
Leitala <yhe7@ualberta.ca>
2022-10-31 05:40:45 -0600

    fix unexpected css issue

--- frontend/my-app/src/views/home/myPost.jsx
+++ frontend/my-app/src/views/home/myPost.jsx
@@ -23,8 +23,7 @@ export default class MyPost extends React.Component {
             <div>
                 <div>
                     {/* <button className="btn">NEW POST</button> */}
-                    <Link to={`./posts`} className="btn">NEW POST</Link>
-                    <Link to={`./Post`} className="btn">NEW IMAGE POST</Link>
+                    <Link to={`./Post`} className="btn">NEW POST</Link>
                 </div>
                 {/* {this.state.seen ? <Post toggle={this.togglePop} /> : null} */}
                 <div>
