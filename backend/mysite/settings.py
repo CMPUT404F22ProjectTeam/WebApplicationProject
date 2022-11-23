8439fceba6d3fe0cc90be00efba55fd071091e48
Yanzhi Huang <yanzhihuang129@gmail.com>
2022-11-22 17:51:02 -0700

    update heroku setting

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'g*vfd9q12!^)i8iauztr4n*mgu9m*xlk5hp8t*7(4(h%+3p_di'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -143,10 +145,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
 ]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 AUTH_USER_MODEL = 'socialdistribution.Author'
 
 CORS_ALLOW_ALL_ORIGINS  = True
@@ -180,5 +185,5 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:8000',
 )
 
-import django_heroku
+
 django_heroku.settings(locals())
