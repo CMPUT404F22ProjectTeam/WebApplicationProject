36467fd52791652152641c84102bba09d5a90c41
kkli08 <ke_2021@126.com>
2022-11-23 23:05:42 -0700

    deploy

--- backend/socialdistribution/viewsets/urlhandler.py
+++ backend/socialdistribution/viewsets/urlhandler.py
@@ -1,15 +1,16 @@
 from urllib.parse import urlparse
 
+
 def get_Safe_url(url):
     url = urlparse(url)
     host = ''
-    if url.port: # exist port
+    if url.port:  # exist port
         host = '{url.scheme}://{url.hostname}:{url.port}'.format(url=url)
     else:
         # port does not exist
-      if url.scheme == 'http':
-          host = 'http://{url.hostname}'.format(url=url)
-      else:
-          host = 'https://{url.hostname}'.format(url=url)
+        if url.scheme == 'http':
+            host = 'http://{url.hostname}'.format(url=url)
+        else:
+            host = 'https://{url.hostname}'.format(url=url)
 
     return host
