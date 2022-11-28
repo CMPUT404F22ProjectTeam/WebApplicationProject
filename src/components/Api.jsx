119acd99b384e0bdc6830549d82f942d20f50133
CharlotteZsm <simiao4@ualberta.ca>
2022-11-28 01:05:58 -0700

    postList

---
+++ src/components/Api.jsx
@@ -0,0 +1,22 @@
+import { Base64 } from 'js-base64';
+
+/**
+ * Get request with JWT token
+ * @param {string} uri URL to get
+ * @returns data from the response
+ */
+export const get = (uri) => {
+    let username = 'charlotte';
+    let password = '12345678';
+    let headers = new Headers();
+    headers.set('Authorization', 'Basic ' + Base64.encode(username + ":" + password));
+    return fetch(uri, {
+        method: 'GET',
+        headers: headers,
+    }).then(async (response) => {
+        if (response.ok) {
+            return response.json()
+        }
+        throw new Error((await response.json()).message)
+    })
+}
