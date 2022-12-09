cd08a5a49fb0a0ec71a68ac6f8df019908055c12
Leitala <yhe7@ualberta.ca>
2022-12-09 11:41:52 -0700

    fix bug

--- src/components/Post/PostList.jsx
+++ src/components/Post/PostList.jsx
@@ -32,11 +32,11 @@ function PostList() {
     useEffect(() => {
         axios.get(`${t06_url}/authors/`, { auth: { username: 'charlotte', password: '12345678' } })
             .then((res) => {
-                setAuthors6(res.items)
+                setAuthors6(res.data.items)
                 authors6.forEach((author) => {
                     axios.get(`${author.id}/posts/`, { auth: { username: 'charlotte', password: '12345678' } })
                         .then((res) => {
-                            res.items.forEach((post) => {
+                            res.data.items.forEach((post) => {
                                 var index = JSON.stringify(postData6).indexOf(JSON.stringify(post))
                                 if (post.visibility === "PUBLIC" && index === -1) {
                                     setPostData6(postData6 => [...postData6, post])
